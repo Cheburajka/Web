ab95b52c0d8414a2249e2cbaa355d6088db4a8c1
Пономарев Антон <79903945+Cheburajka@users.noreply.github.com>
2024-09-24 12:51:59 +0300

    Задание 2
    Создание сайт-визитки

---
+++ mouse.js
@@ -0,0 +1,38 @@
+const element = document.getElementById('phone');
+const el = document.getElementById('ph');
+
+        element.addEventListener('mouseover', function(event) {
+        el.style.color = 'black';
+        event.target.style.fontWeight = 'bold';
+        });
+        
+        element.addEventListener('mouseout', function(event) {
+        el.style.color = 'white';
+        event.target.style.fontWeight = 'normal';
+        });
+        
+const element2 = document.getElementById('telegram');
+const el2 = document.getElementById('tg');
+        
+        element2.addEventListener('mouseover', function(event) {
+        el2.style.color = 'black';
+        event.target.style.fontWeight = 'bold';
+        });
+        
+        element2.addEventListener('mouseout', function(event) {
+        el2.style.color = 'white';
+        event.target.style.fontWeight = 'normal';
+        });
+        
+const element3 = document.getElementById('email');
+const el3 = document.getElementById('em');
+        
+        element3.addEventListener('mouseover', function(event) {
+        el3.style.color = 'black';
+        event.target.style.fontWeight = 'bold';
+        });
+        
+        element3.addEventListener('mouseout', function(event) {
+        el3.style.color = 'white';
+        event.target.style.fontWeight = 'normal';
+        });
